9b139d37a6bf53f3516c04044b2664959ddd9353
Daniel Barahona <>
2021-04-13 23:16:36 +0000

    Completed

--- index.js
+++ index.js
@@ -12,10 +12,33 @@ The output should be a number.
 
 */
 
-function genderCount(array, string){
+function genderCount(array, genderEnter){
 //CODE BELOW HERE
 
+//to hold the count
+var count = 0;
+
+//iterating over the array 
+for(var i = 0; i <= array.length - 1; i++){
 
 
-//CODE ABOVE HERE
+//What condition are we checking for?
+//What needs to be true?
+if(array[i].gender === genderEnter) {
+   
+   //add one to count when the input gender matches the one in the dataset
+    
+    
+    count++ 
+  
+  
+  }
+
 }
+
+
+console.log(count)
+} 
+//CODE ABOVE HERE
+ 
+  genderCount(customers, 'male')
